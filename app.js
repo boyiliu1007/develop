2d10340e187d4cb25110a8776b35a7251ca6786d
boyiliu1007 <boyiliu1007@gmail.com>
2023-02-03 22:51:56 +0800

    backend modify

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./develop.db');
+const loginAccount = 'SELECT * FROM ACCOUNT WHERE ACC_UN = ? AND ACC_PW = ?';
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+//const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const app = express();
+const corsConfig = {
+    credentials: true,
+    origin: true,
+};
+   
+app.use(cors(corsConfig))
+app.use(logger('dev')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//=======================================================
+console.log('Console says hi!')
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    db.each(loginAccount, req.body.username,req.body.username, function (err, row) {
+        console.log('Welcome '+row.username);
+    });
+    db.run(loginAccount, [req.body.username,req.body.username]);
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
+//完成
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/get/:message/',function(req,res,next){
-  res.send(req.params.message+req.query.year);
+//原本的
+// router.get('/',function(req,res,next){
+//   res.send(req.params.message+req.query.year);
+// });
+// router.post('/post',function(req,res,next){
+//   res.send(req.body.message);
+// });
+
+//學長的
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+router.get('/', function (req, res, ) {
+  res.send('Hello World!');
+  db.each(loginAccount, req.body.username,req.body.username, function (err, row) {
+      console.log('Welcome '+row.username);
+  });
+  db.run(loginAccount, [req.body.username,req.body.username]);
 });
 
-// module.exports = router;(原本在這裡不知道要不要刪)
-router.post('/post',function(req,res,next){
-  res.send(req.body.message);
-});
+module.exports = router;
