b93eff90801d8ec05e99804271ce216bb61475ca
boyiliu1007 <boyiliu1007@gmail.com>
2023-04-17 14:35:31 +0800

    not login RWD

--- frontend/src/views/NotLogin.vue
+++ frontend/src/views/NotLogin.vue
@@ -1,6 +1,6 @@
 <!-- done -->
 <template>
     <div class = "fontSize animate__animated animate__shakeX">
-      <h1>You're not logged in. Please log in first.</h1>
+      <h1 class = "px-5">You're not logged in. Please log in first.</h1>
     </div>
 </template>
